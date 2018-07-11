8a415d721afbf0059961c73ffaf8f7ce7593e268
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-11 19:17:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ function ternaryCheckCity(city){
 function switchOnCharmFromTip(tip){
   switch (tip){
     case 'generous':
-      return 'Thank you so much.'
-    case 'Thank you so much.'  
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you';
+    case 
 }
