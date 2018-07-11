fd174083edb701472d9dfa39950004de036eee52
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-11 18:23:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-var feet=400
-function scuberGreetingForFeet(){
+var feet=400;
+function scuberGreetingForFeet(feet){
   if(feet<=400){
     console.log('This one is on me!')
   }
