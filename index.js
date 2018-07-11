d5517ea2041c2356a755e7c7336819bfa264bbc9
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-11 18:55:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Write your code in this file!
 
 function scuberGreetingForFeet(feet) {
-  if (feet > 2000) {
+  
+  if(){}
+  else if (feet > 2000) {
     return 'I will gladly take your thirty bucks.';
   } else if (feet >= 2500) {
     return 'No can do.';
