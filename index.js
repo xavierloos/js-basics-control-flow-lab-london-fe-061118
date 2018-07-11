7a639dd3d861889f1e215ab87352d15581de35dd
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-11 18:38:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // Write your code in this file!
 var feet=199;
 function scuberGreetingForFeet(feet){
-    return('This one is on me!')
-   
+  return('This one is on me!')
 }
 feet=2001
 function scuberGreetingForFeet(feet){
-    
-      console.log('I will gladly take your thirty bucks.')
+  console.log('I will gladly take your thirty bucks.')
     
 }
