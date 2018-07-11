d22c72fa58221e998d15b539d4415cccf930a688
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-11 18:39:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,3 @@ var feet=199;
 function scuberGreetingForFeet(feet){
   return('This one is on me!')
 }
-feet=2001
-function scuberGreetingForFeet(feet){
-  console.log('I will gladly take your thirty bucks.')
-    
-}
