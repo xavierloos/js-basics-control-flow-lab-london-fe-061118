a41e6a75eff6af3eed8e37cc01ba990a43e48078
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-11 18:50:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(feet){
-  return('This one is on me!')
-  if (feet>2001){
-    console.log('I will gladly take your thirty bucks.')
+function scuberGreetingForFeet(feet) {
+  if (feet > 2001) {
+    return 'I will gladly take your thirty bucks.';
   }
+  
 }
