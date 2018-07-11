efa9c6dfce7ee280e1def373cc4281219dac8050
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-11 18:40:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var feet=199;
 function scuberGreetingForFeet(feet){
   return('This one is on me!')
+  if (scuberGreetingForFeet>2001){
+  return()
 }
-if (scuberGreetingForFeet>2001){
-  return
 }
