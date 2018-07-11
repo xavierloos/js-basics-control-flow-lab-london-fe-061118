e4f538d7931c329d50b190a8bf9f430233da9514
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-11 18:56:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 
 function scuberGreetingForFeet(feet) {
   
-  if(){}
-  else if (feet > 2000) {
+  if(feet>199){
+    return 'I will gladly take your thirty bucks.';
+  }else if (feet > 2000) {
     return 'I will gladly take your thirty bucks.';
   } else if (feet >= 2500) {
     return 'No can do.';
