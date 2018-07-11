863c8e9866351acd285f4ffe9bbbb1e1fa0704c3
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-11 18:52:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ function scuberGreetingForFeet(feet) {
   if (feet > 2000) {
     return 'I will gladly take your thirty bucks.';
   }
+   if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
   
 }
