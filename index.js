21a19b66b7f9e8110037e5d88fe72ea3292bb1b5
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-11 19:19:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(feet) {
+  if(feet<=400){
+    return 'This one is on me!';
+  }else if (feet > 2000 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city){
+  return city == 'NYC' ?  'Ok, sounds good.' : 'No go.' ;
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.'
+}
+}
